85caa8d90e014e7615aad9e2bcb457a9fc2f6eb4
ddanilooalves <daniloalves0512@gmail.com>
2022-06-02 21:13:36 -0300

    fix(games): import `PrismaModule` on `GamesModule`

--- src/users/dto/create-users.dto.ts
+++ src/users/dto/create-users.dto.ts
@@ -2,13 +2,47 @@ import { IsNumber, IsPositive, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUsersDto {
+  @IsNumber()
+  @IsPositive()
   @ApiProperty({
-    description: 'Insira os dados',
+    description: 'Insira o número',
+    example: 1
   })
   number: number;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Insira o nome',
+    example: "Danilo"
+  })
   name: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Insira o e-mail',
+    example: "daniloalves@gmail.com"
+  })
   email: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Insira a senha',
+    example: "12345a"
+  })
   password: string;
+
+  @IsNumber()
+  @IsPositive()
+  @ApiProperty({
+    description: 'Insira o número',
+    example: 12345678910
+  })
   cpf: number;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Insira o nome de usuário administrador',
+    example: "XunimDelirium22"
+  })
   isAdmin: string
 }
--- src/users/dto/update-table.dto.ts
+++ src/users/dto/update-table.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateUsersDto } from './create-users.dto';
 
 export class UpdateUsersDto extends PartialType(CreateUsersDto) {}
