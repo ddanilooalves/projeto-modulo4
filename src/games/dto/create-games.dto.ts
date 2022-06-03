c366eeac1990460080c52fd5ed87ae7a16b0a5e1
ddanilooalves <daniloalves0512@gmail.com>
2022-06-02 21:05:19 -0300

    feat(games): implement dto validation

--- src/games/dto/create-games.dto.ts
+++ src/games/dto/create-games.dto.ts
@@ -2,14 +2,54 @@ import { IsNumber, IsPositive, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateGamesDto {
+  @IsString()
   @ApiProperty({
-    description: 'Insira os dados',
+    description: 'Insira o nome do jogo',
+    example: "Pac Man"
   })
   name: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Insira a URL da imagem do jogo',
+    example: "...."
+  })
   coverImageUrl: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Insira a descrição do jogo',
+    example: "Jogo retrô de fliperama..."
+  })
   description: string;
+
+  @IsNumber()
+  @IsPositive()
+  @ApiProperty({
+    description: 'Insira o ano que o jogo foi criado',
+    example: 2022
+  })
   year: number;
+
+  @IsNumber()
+  @IsPositive()
+  @ApiProperty({
+    description: 'Insira o score do jogo',
+    example: 10
+  })
   imdbScore: number;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Insira a URL do YouTube do trailer do jogo',
+    example: "..."
+  })
   trailerYouTubeUrl: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Insira a URL do YouTube da jogabilidade do jogo',
+    example: "..."
+  })
   gameplayYouTubeUrl: string
 }
