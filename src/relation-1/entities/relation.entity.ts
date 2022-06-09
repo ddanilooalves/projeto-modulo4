85240443b8dfcf56efb85bde3f83988ca3304c92
ddanilooalves <daniloalves0512@gmail.com>
2022-06-08 22:33:38 -0300

    correction

--- src/relation-1/entities/relation.entity.ts
+++ src/relation-1/entities/relation.entity.ts
@@ -2,8 +2,8 @@ import { Games } from "@prisma/client";
 
 export class Relation {
   id?: string;
-  gendersId?: string;
-  profileId?: string;
+  gendersId: string;
+  profileId: string;
   createdAt?: Date;
   updatedAt?: Date;
   games?: Games[]
