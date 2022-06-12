1920a50c80b4a24d6b75422569fb2dc83e38a479
ddanilooalves <daniloalves0512@gmail.com>
2022-06-12 19:14:15 -0300

    feat(auth): implement strategy and guards

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { PrismaService } from "prisma/prisma/prisma.service";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpirtation: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: { email: string }) {
+    const user = await this.prisma.user.findUnique({ 
+      where: { email: payload.email },
+    });
+
+    if (!user) {
+        throw new UnauthorizedException('Usuário não existe ou não está autorizado');
+      }
+
+    delete user.password;
+    return user;
+  }
+}
