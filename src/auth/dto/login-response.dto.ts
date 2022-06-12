3ec76909cf2f141f8e4bcf9248cb8939685d2da7
ddanilooalves <daniloalves0512@gmail.com>
2022-06-12 18:22:57 -0300

    feat(auth): implement login

--- src/auth/dto/login-response.dto.ts
+++ src/auth/dto/login-response.dto.ts
@@ -4,7 +4,7 @@ import { User } from "@prisma/client";
 export class LoginResponseDto {
     @ApiProperty({
         description: 'JWT gerado pelo login',
-        example: 'Token_gerado_Automaticamente',
+        example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRhbmlsb2FsdmVzQGdtYWlsLmNvbSIsImlhdCI6MTY1NTA2ODkwOCwiZXhwIjoxNjU1MTU1MzA4fQ.KNZsK87JZi9JmJJB5DDcWbpYbis0YHtBz2HH_BIjTcE',
     })
     token: string;
 
